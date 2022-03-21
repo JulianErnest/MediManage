4dd54735ccf553f1b43070986b412bc2469905f3
Zecharia123z <barrigazakar@gmail.com>
2022-03-21 10:58:55 +0800

    hello

---
+++ Login.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Login</title>
+        <link rel="stylesheet" href="Login.css">
+    </head>
+    
+    <body>
+        <center>
+        <div class="title"><br><br>MediManage
+            <img src="med.jpg">
+        </div>
+        <div class="forms">
+            <form>
+            <div class="Email">
+                    <p1>Email: </p1>
+                    <input type="text" class="input-field1">
+                </div>
+                <div class="Password">
+                    <p2>Password: </p2>
+                    <input type="text" class="input-field2">
+                </div>
+            </form>
+        </div>
+
+        <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"><br>Log-in<br><br></a>
+        
+        <button class="button">Sign-up</button>
+        </center>
+    </body>
+</html>
